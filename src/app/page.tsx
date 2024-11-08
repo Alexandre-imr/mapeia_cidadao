1f52c05c20406bdfb236ab285d167151b9fdc814
Alexandre Rabelo <alexandre.imr@hotmail.com>
2024-11-08 18:39:05 -0300

    https://github.com/Alexandre-imr/mapeia_cidadao.git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,76 @@
-import { Button, Card, CardBody, Stack, Heading, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  Heading,
+  Image,
+} from "@chakra-ui/react";
+import Link from "next/link";
 
-function Page() {
+export default function LoginPage() {
   return (
-    <Card maxW="sm">
-      <CardBody>
-        <Stack mt="6" spacing="3">
-          <Heading size="md">Living room Sofa</Heading>
-          <Text>
-            This sofa is perfect for modern tropical spaces, baroque inspired
-            spaces, earthy toned spaces and for people who love a chic design
-            with a sprinkle of vintage design.
-          </Text>
-          <Text color="blue.600" fontSize="2xl">
-            $450
-          </Text>
-        </Stack>
-      </CardBody>
-    </Card>
+    <Flex
+      height="100vh"
+      align="center"
+      justify="center"
+      bgGradient="linear(to-br, purple.800, pink.400)"
+    >
+      <Box
+        bg="black"
+        borderRadius="lg"
+        p={8}
+        width={{ base: "90%", md: "400px" }}
+        textAlign="center"
+        boxShadow="lg"
+      >
+        <Image src="/Logo.png" alt="Logo" mb={4} mx="auto" />
+
+        <Heading as="h2" size="lg" color="white" mb={6}>
+          Login
+        </Heading>
+
+        <FormControl id="email" mb={4}>
+          <FormLabel color="white" textAlign="left">
+            Email
+          </FormLabel>
+          <Input
+            type="email"
+            placeholder="Email"
+            bg="gray.700"
+            color="white"
+            _placeholder={{ color: "gray.500" }}
+          />
+        </FormControl>
+
+        <FormControl id="password" mb={4}>
+          <FormLabel color="white" textAlign="left">
+            Senha
+          </FormLabel>
+          <Input
+            type="password"
+            placeholder="Senha"
+            bg="gray.700"
+            color="white"
+            _placeholder={{ color: "gray.500" }}
+          />
+        </FormControl>
+
+        <Button colorScheme="green" width="full" mt={4} mb={4}>
+          Entrar
+        </Button>
+
+        {/* Link para Cadastro */}
+        <Text color="white">
+          Não tem uma conta?
+          <Link href="/register" color="green">
+            Cadastra-se
+          </Link>
+        </Text>
+      </Box>
+    </Flex>
   );
 }
-
-export default Page;
---
+++ src/app/register.tsx
@@ -0,0 +1,71 @@
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Text,
+  Link,
+  Heading,
+  Image,
+} from "@chakra-ui/react";
+
+export default function Page() {
+  return (
+    <Flex height="100vh">
+      {/* Container da Imagem */}
+      <Box flex="1" bg="gray.200" display={{ base: "none", md: "block" }}>
+        <Image src="/Capa.png" alt="Capa" w="100%" h="100%" />
+      </Box>
+
+      {/* Container do Formulário */}
+      <Flex
+        flex="1"
+        bgGradient="linear(to-r, purple.800, purple.400)"
+        align="center"
+        justify="center"
+      >
+        <Box>
+          <Image src="/Logo.png" alt="Capa" />
+          <Flex direction="column" align="center" mb="4">
+            <Heading as="h2" size="lg" color="white">
+              MAPEIA CIDADÃO
+            </Heading>
+            <Text fontSize="xl" color="white" mt="4" mb="8">
+              Cadastre-se
+            </Text>
+          </Flex>
+
+          <FormControl id="email" mb="4">
+            <FormLabel>Email</FormLabel>
+            <Input
+              type="email"
+              bg="purple.900"
+              color="white"
+              placeholder="Digite seu email"
+            />
+          </FormControl>
+
+          <FormControl id="password" mb="4">
+            <FormLabel>Senha</FormLabel>
+            <Input
+              type="password"
+              bg="purple.900"
+              color="white"
+              placeholder="Digite sua senha"
+            />
+          </FormControl>
+
+          <Button colorScheme="green" width="full" mt="4">
+            Entrar
+          </Button>
+
+          <Text textAlign="center" mt="4" color="white">
+            Não tem uma conta?{" "}
+          </Text>
+        </Box>
+      </Flex>
+    </Flex>
+  );
+}
